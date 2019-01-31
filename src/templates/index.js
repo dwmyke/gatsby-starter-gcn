46ed9b7505c69e812f6ca514318121fab98db68f
Mikel-Jon West <dwmyke@gmail.com>
2019-01-31 09:09:40 -0800

    Update index.js

--- src/templates/index.js
+++ src/templates/index.js
@@ -31,7 +31,6 @@ const Index = ({ data, pageContext }) => {
       <Card><div style={{textAlign: `center`, position: `relative`, paddingBottom: `56.25%`, height: `0`, overflow: `hidden`, maxWidth: `100%`}}> 
       <iframe src='https://www.youtube.com/embed/Md6fHL6lxYk' frameborder='0' allowfullscreen style={{position: `absolute`, top: `0`, left: `0`, width: `100%`, height: `100%`}}></iframe>
       </div>
-      </div>
       </Card>
       <Container>
         {isFirstPage ? (
