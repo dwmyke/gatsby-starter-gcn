6619a7ba3800d23c96e8a06287b5873b91c2e4e8
Mikel-Jon West <dwmyke@gmail.com>
2019-03-19 14:36:29 -0700

    Update Banner.js

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -19,7 +19,7 @@ const Wrapper = styled.div`
 const Banner = () => (
   <Wrapper>
     <PageTitle>Mikel-Jon (Mike) West</PageTitle>
-      <PageSub>Writer/Director • Video Editor • Visual Designer • Technical Developer</PageSub>
+      <PageSub><a href="https://mikewest.pro/writer-director/>Writer/Director</a> • Video Editor • Visual Designer • Technical Developer</PageSub>
       <Container><div style={{textAlign: `center`, position: `relative`, paddingBottom: `56.25%`, height: `0`, overflow: `hidden`, maxWidth: `100%`}}> 
       <iframe src='https://www.youtube.com/embed/Md6fHL6lxYk' frameborder='0' allowfullscreen style={{position: `absolute`, top: `0`, left: `0`, width: `100%`, height: `100%`}}></iframe>
       </div>
