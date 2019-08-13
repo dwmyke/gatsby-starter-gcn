2214489b6db3d4a10e4a14d7265db76743313197
Mikel-Jon West <dwmyke@gmail.com>
2019-08-13 10:00:35 -0700

    Updated Banner.js for quick demo reel on Vimeo.

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -21,7 +21,7 @@ const Banner = () => (
     <PageTitle>Mikel-Jon (Mike) West</PageTitle>
       <BannerSub><a href="https://mikewest.pro/writer-director">Writer/Director</a> • <a href="https://mikewest.pro/video-editor">Video Editor</a> • <a href="https://mikewest.pro/visual-designer">Visual Designer</a> • <a href="https://mikewest.pro/technical-developer">Technical Developer</a></BannerSub>
       <Container><div style={{textAlign: `center`, position: `relative`, paddingBottom: `56.25%`, height: `0`, overflow: `hidden`, maxWidth: `100%`}}> 
-      <iframe src='https://www.youtube.com/embed/Md6fHL6lxYk' frameborder='0' allowfullscreen style={{position: `absolute`, top: `0`, left: `0`, width: `100%`, height: `100%`}}></iframe>
+      <iframe src='https://player.vimeo.com/video/330364561' frameborder='0' webkitAllowFullScreen mozallowfullscreen allowFullScreen style={{position: `absolute`, top: `0`, left: `0`, width: `100%`, height: `100%`}}></iframe>
       </div>
       </Container>
   </Wrapper>
