4c5d364676641f821a05bc860b015191af44663f
Mikel-Jon West <dwmyke@gmail.com>
2021-02-26 00:55:47 -0800

    Took out favicon from layout.

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import { ThemeProvider } from 'styled-components'
 import Helmet from 'react-helmet'
-import favicon from '../images/favicon.ico'
+// import favicon from '../images/favicon.ico'
 import GlobalStyle from '../styles/global'
 import theme from '../styles/theme'
 import config from '../utils/siteConfig'
@@ -15,7 +15,7 @@ const Template = ({ children }) => {
         <title>{config.siteTitle}</title>
         <meta charSet="utf-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <link rel="icon" href={favicon} />
+        {/* <link rel="icon" href={favicon} /> */}
       </Helmet>
 
       <ThemeProvider theme={theme}>
